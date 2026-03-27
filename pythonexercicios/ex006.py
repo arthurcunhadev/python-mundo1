a9bf34dd5e9e96c5e7ae1659da0ca34028ccd9e7
Arthur Cunha Nonato <contato.arthurnonato@gmail.com>
2026-03-26 23:16:25 -0300

    estudo de operadores aritméticos

--- pythonexercicios/ex006.py
+++ pythonexercicios/ex006.py
@@ -1,3 +1,2 @@
 n=int(input('digite um número: '))
-print(n+1,n-1)
-
+print(n*2, n*3, n**(1/2))
---
+++ pythonexercicios/ex007.py
@@ -0,0 +1,5 @@
+n1=int(input('primeira avaliação: '))
+n2=int(input('segunda avaliação: '))
+s=n1+n2
+m=s/2
+print(f'A média é {m}')
