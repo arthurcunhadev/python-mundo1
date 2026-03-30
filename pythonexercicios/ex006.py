0e13c7863b683149f77ea749e6773d6b0815646c
Arthur Cunha Nonato <contato.arthurnonato@gmail.com>
2026-03-30 20:48:55 -0300

    update de exercícios

--- pythonexercicios/ex006.py
+++ pythonexercicios/ex006.py
@@ -1,2 +1,2 @@
 n=int(input('digite um número: '))
-print(f'dobro {n*2}.\ntriplo {n*3}.\nraiz quadrada {n**(1/2)}')
+print(f'dobro {n*2}.\ntriplo {n*3}.\nraiz quadrada {n**(1/2):.2f}')
--- pythonexercicios/ex013.py
+++ pythonexercicios/ex013.py
@@ -1,3 +1,3 @@
 s=float(input('quanto é o salário?: '))
 a=s*1.15
-print(f'o novo salário: {a}')
+print(f'o novo salário: {a:.2f}')
